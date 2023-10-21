08c77dac0686c87031ddb2618603de0cc5b27c3a
Anoop N <anoop.n@ovhcloud.com>
2023-10-22 00:41:59 +0530

    feat: initial commit

---
+++ App.js
@@ -0,0 +1,13 @@
+// import React from 'react';
+import ReactDOM from "react-dom/client";
+
+const App = () => {
+  return (
+    <>
+      <h1>Hello from React App 🚀</h1>
+      <h2>Binge watching React and killin' it</h2>
+    </>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
